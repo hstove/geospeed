952b3fe4fb96e957098b47e4ffb0dc5576690079
hank <hstove@gmail.com>
2015-02-16 10:02:03 -0800

    fixes controller typo bug

--- app/controllers/speed_limit_controller.rb
+++ app/controllers/speed_limit_controller.rb
@@ -1,6 +1,6 @@
 class SpeedLimitController < ApplicationController
   def show
-    speed = Rails.cache.fetch(['maxspeed', params[:latitude], params[:longitude]) do
+    speed = Rails.cache.fetch(['maxspeed', params[:latitude], params[:longitude]]) do
       Overpass.maxspeed(params[:latitude].to_f, params[:longitude].to_f)
     end
     render json: { maxspeed: speed }
