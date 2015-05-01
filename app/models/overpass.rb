820a045cb543e28354f0c667a57697f3001df6bb
hank <hstove@gmail.com>
2015-05-01 12:53:53 -0700

    don't show speed limit unless close enough

--- app/models/overpass.rb
+++ app/models/overpass.rb
@@ -5,6 +5,7 @@ class Overpass
 
       page = Nokogiri::HTML(HTTParty.get(url))
       closest = closest_node_id(page, lat, lng)
+      ap closest
       way = page.css("way nd[ref='#{closest}']")[0].try(:parent)
 
       response = {
@@ -30,25 +31,51 @@ class Overpass
       distances = {}
       nodes = page.css('node')
       nodes = nodes.to_a.sort do |node1, node2|
-        distance1 = distance_from_node(node1, lat, lng)
-        distance2 = distance_from_node(node2, lat, lng)
 
-        node = nodes.select {|n| n['id'] == node1['id'] }[0]
-        distances[node['id']] ||= distance1 if node
+        unless distance1 = distances[node1['id']]
+          distance1 = distance_from_node(node1, lat, lng)
+          node = nodes.select {|n| n['id'] == node1['id'] }[0]
+          distances[node['id']] ||= distance1 if node
+        end
+        unless distance2 = distances[node2['id']]
+          distance2 = distance_from_node(node2, lat, lng)
+          node = nodes.select {|n| n['id'] == node2['id'] }[0]
+          distances[node['id']] ||= distance2 if node
+        end
 
         distance1 <=> distance2
       end
 
       node = nodes.first
-      Rails.logger.info "Node Distance - #{distances[node['id']]}" if node
-      node ? nodes.first["id"] : ''
+      if node
+        distance = distances[node['id']]
+        Rails.logger.info "Node Distance - #{distance}"
+        if distance < 200
+          node['id']
+        else
+          ''
+        end
+      else
+        ''
+      end
     end
 
+    # returns the distance of two points in meters
+    # https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
     def distance_from_node(node, lat, lng)
-      distance = 0
-      distance += (node["lat"].to_f.abs - lat.abs).abs
-      distance += (node["lon"].to_f.abs - lng.abs).abs
-      distance
+      # distance = 0
+      # distance += (node["lat"].to_f.abs - lat.abs).abs
+      # distance += (node["lon"].to_f.abs - lng.abs).abs
+      # distance
+      radius = 6378.137 # Radius of earth in KM
+      dLat = (lat - node['lat'].to_f) * Math::PI / 180
+      dLon = (lng - node['lon'].to_f) * Math::PI / 180
+      a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(node['lat'].to_f * Math::PI / 180) * Math.cos(lat * Math::PI / 180) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+      c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+      d = radius * c
+      d * 1000
     end
 
     def url_for(lat, lng)
--- spec/controllers/speed_limit_controller_spec.rb
+++ spec/controllers/speed_limit_controller_spec.rb
@@ -3,12 +3,19 @@ require 'rails_helper'
 RSpec.describe SpeedLimitController, type: :controller do
 
   describe "GET #show", :vcr do
-    it "returns http success" do
-      get :show, latitude: 48.7588802, longitude: -122.4875853
+    it "returns http success when close enough" do
+      get :show, latitude: 48.758551, longitude: -122.482603
       data = JSON.parse(response.body)
       expect(data['maxspeed']).to eq(25)
       expect(data['format']).to eq('mph')
       expect(data['street']).to eq('F Street')
+    end
+
+    it "returns nil when not close enough" do
+      get :show, latitude: 48.7588802, longitude: -122.4875853
+      data = JSON.parse(response.body)
+      expect(data['maxspeed']).to eq(nil)
+      expect(data['format']).to eq(nil)
     end
 
     it "returns nil when not found" do
